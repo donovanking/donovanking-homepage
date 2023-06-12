06319f11cdc91533ade33d25098e203e7cb0b780
donovanking <kinguw@bc.edu>
2023-06-12 15:31:36 -0400

    container

--- pages/404.js
+++ pages/404.js
@@ -3,7 +3,7 @@ import { Box, Heading, Text, Container, Divider, Button } from '@chakra-ui/react
 
 const NotFound = () => {
     return (
-        <Containter>
+        <Container>
             <Heading as="h1">Not Found</Heading>
             <Text> The page you&apos;re looking for was not found.</Text>
             <Divider my={6} />
@@ -13,7 +13,7 @@ const NotFound = () => {
                     <Button colorScheme="teal"> Return to home</Button>
                 </NextLink>
             </Box>
-        </Containter>
+        </Container>
     )
 }
 
